fa1486dd6ecc07df490741eabcb5551a77bb0c03
devnoorfatima <devnoorfatima@gmail.com>
2018-12-13 20:06:28 +0500

    Made an age calculator with basic javascript, that tells how many days you've been alive by entering how old are you now.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var firstName = prompt("Enter your First Name ");
-var secondName = prompt("Enter your Last Name ");
-var age = prompt("Enter your age");
-console .log ("Your full name is " + firstName  + secondName);
-console .log ("Your age is " + age );
+// var firstName = prompt("Enter your First Name ");
+// var secondName = prompt("Enter your Last Name ");
+// var age = prompt("Enter your age");
+// console .log ("Your full name is " + firstName  + secondName);
+// console .log ("Your age is " + age );
+
+// age calculator:
+ var age = prompt ("What is your age?");
+ console.log(365 * age);
