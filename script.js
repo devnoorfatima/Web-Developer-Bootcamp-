2c5e00128edcaa2234250e085fb148bbb03ea0f4
devnoorfatima <devnoorfatima@gmail.com>
2018-12-13 19:59:38 +0500

    Used prompt to get the data of the user.

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter your First Name ");
+var secondName = prompt("Enter your Last Name ");
+var age = prompt("Enter your age");
+console .log ("Your full name is " + firstName  + secondName);
+console .log ("Your age is " + age );
