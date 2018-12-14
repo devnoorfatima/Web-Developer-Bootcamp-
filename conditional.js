2fff5c660b2aa92be24b017a8db1e62feffde4c5
devnoorfatima <devnoorfatima@gmail.com>
2018-12-14 22:09:40 +0500

    Practised conditionals and if/else statements. Made a number guessing game.

--- conditional.js
+++ conditional.js
@@ -1,10 +1,23 @@
-var age = prompt ("Enter your age.");
-if (age < 0) {
-    console .log(" error");
+// var age = prompt ("Enter your age.");
+// if (age < 0) {
+//     console .log(" error");
+// }
+// else if( age === 21) {
+//     console.log("Happy 21st Birthday.");
+// }
+// else if (age%2 === 1) {
+//     console .log("Your age is odd."); 
+// }
+
+// Number guessing game
+var secretNumber = 5;
+var number = prompt("Enter a number here.");
+if (number === secretNumber){
+    console.log("You guessed the number.");
 }
-else if( age === 21) {
-    console.log("Happy 21st Birthday.");
+else if(number < secretNumber ){
+    console.log("Too low try again!!");
 }
-else if (age%2 === 1) {
-    console .log("Your age is odd."); 
+else if (number > secretNumber) {
+    console .log("Too high try again");
 }
