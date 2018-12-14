99fbb82344669a7808cb87a8b80fd61c8764058c
devnoorfatima <devnoorfatima@gmail.com>
2018-12-14 11:53:13 +0500

    Practised conditionals and if/else statements.

---
+++ conditional.js
@@ -0,0 +1,10 @@
+var age = prompt ("Enter your age.");
+if (age < 0) {
+    console .log(" error");
+}
+else if( age === 21) {
+    console.log("Happy 21st Birthday.");
+}
+else if (age%2 === 1) {
+    console .log("Your age is odd."); 
+}
